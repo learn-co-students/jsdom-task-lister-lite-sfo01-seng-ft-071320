a548a6c485ded07a89f0eb2a38968061b2c5c03c
Taci Shlosberg <tshlosberg@gmail.com>
2020-08-25 16:08:15 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  buttonClick();
+  addTask();
 });
+
+function buttonClick() {
+  const button = document.querySelector("input[type=submit]");
+
+  button.addEventListener("click", function(event) {
+  });
+}
+
+function addTask() {
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const input = document.getElementById("new-task-description");
+
+    // const commentInput = document.createTextNode(`${input}.value`);
+    const commentInput = event.target[0];
+
+    const textBox = commentInput.value;
+    const comment = document.createElement("li");
+    comment.textContent = textBox;
+
+    const commentsContainer =  document.getElementById("tasks");
+    const list = commentsContainer.appendChild(comment);
+    const button = document.createElement("button");
+    button.innerHTML = "x"
+    list.append(button);
+
+    list.addEventListener("click", function() {
+      list.remove();
+    })
+
+    commentInput.value = "";
+ 
+  });
+}
+
+
+
+
